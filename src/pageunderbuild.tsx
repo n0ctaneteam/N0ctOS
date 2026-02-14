175f94ed493b3fd7b9cbb65e9ad681b5976e074b
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-02-14 07:05:24 +0530

    added page under build

---
+++ src/pageunderbuild.tsx
@@ -0,0 +1,25 @@
+import logo from './assets/N0ctOS.png'
+
+export const Pageunderbuild = () => {
+    return (
+        <div
+            className="
+            flex
+            items-center
+            justify-around
+            flex-col
+            bg-slate-950
+            from-gray-50
+            font-tektur
+            font-black
+            text-center
+            text-8xl
+            min-h-dvh
+            text
+            "
+        >
+            <img src={logo}/>
+            <h1>Page Under Build..</h1>
+        </div>
+    );
+};
