52b5a8f2305802246e8431ac5e0bc14b896f9c84
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-03-27 14:33:40 +0530

    Change h2 to h1 for skeleton message display

--- src/pageunderbuild.tsx
+++ src/pageunderbuild.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+1import { motion } from "framer-motion";
 
 // import "/test.css";
 
@@ -56,7 +56,7 @@ export const Pageunderbuild = () => {
             <h1 className="text-[clamp(2.5rem,10vw,4.8rem)] leading-none bg-gradient-to-br from-violet-600 via-purple-600 to-purple-500 bg-clip-text text-transparent">
               {randomMessage}
             </h1>
-            <h2>☠️💀☠️</h2>
+            <h1 className="text-[clamp(2rem,8vw,4rem)]>☠️💀☠️</h1>
             <div>
               <span>Meanwhile, Checkout our </span>
               <a
