9cbda206ff7a7e5ecfb500691f33a573c24c12ea
anaghsinghcodingo <130284948+anaghsinghcodingo@users.noreply.github.com>
2026-03-31 21:45:11 +0530

    Change isPublic to true and update component imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-const isPublic: boolean = false;
+const isPublic: boolean = true;
 // this variable decides if the wall is to be shown or not...
 // true: publishes the app to isPublic
 // false: replaces everything with wall(pageunderbuild)
@@ -12,15 +12,20 @@ import "./index.css";
 
 // Import pages
 import { App } from "./App";
-import { Pageunderbuild } from "./pageunderbuild";
+import { PageUnderBuild } from "./pageunderbuild";
+import CustomCursor from "./components/CustomCursor";
 import ScrollToTop from "./components/ScrollToTop";
 
 // import './test.css'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/N0ctOS">
-      <><ScrollToTop/>{isPublic ? <App /> : <Pageunderbuild />}</> {/*decides what to render*/}
+    <BrowserRouter>
+      <>
+        {isPublic ? <App /> : <PageUnderBuild />}
+        <CustomCursor />
+        <ScrollToTop />
+      </>
     </BrowserRouter>
   </StrictMode>,
 );
