01865e5def97457303eff5ee5a608bc53eeec1d1
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-01 02:41:15 +0530

    Add Navigate import from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
