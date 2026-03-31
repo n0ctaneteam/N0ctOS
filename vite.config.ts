3a666ea442cf16cbf5c04462c851d04e6864bf7b
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-01 02:02:01 +0530

    Vit config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+
+export const isPublic = false
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -7,6 +10,10 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: "/N0ctOS",
+    server: {
+      host: true
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
